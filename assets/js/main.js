d7d5ea736e972a68104ac623e34d51959e61eb33
githubsupercode <alex.supercode@gmail.com>
2022-05-19 13:01:07 +0200

    firstpush

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+// Arrays2 1.1
+
+// let languages = [
+//     "JavaScript",
+//     "Python",
+//     "Java",
+//     "Ruby",
+//     "PHP",
+//     "C++",
+//     "CSS",
+//     "C#",
+//     "Go",
+//     "C",
+//     "TypeScript",
+//     "Swift"
+// ];
+
+// languages.sort();
+// console.log(languages);
+
+// Arrays2 1.2
+
+// let languages = [
+//     "JavaScript",
+//     "Python",
+//     "Java",
+//     "Ruby",
+//     "PHP",
+//     "C++",
+//     "CSS",
+//     "C#",
+//     "Go",
+//     "C",
+//     "TypeScript",
+//     "Swift"
+// ];
+
+// let sortierung2 = languages.sort();
+
+// sortierung2.reverse();
+
+// console.log(sortierung2);
+
+// Arrays2 2.2
+
+// let numArray2 = [36, 324, 122, 62, 98, 88, 99, 1000];
+
+let numArray2 = [36, 324, 122, 62, 98, 88, 99, 1000];
+numArray2.sort((a,b) => a - b);
+console.log(numArray2);
+
+// Arrays2 1.13
+
+let meinText1 = ["Hello", "World"];
+let meinText2 = ["Anass", "Elaine", "Eric", "Georg"];
+let meinText3 = ["Superman", "Wonderwoman", "Hulk", "Batman", "Spiderman"];
+
+join1 = meinText1.join();
+join2 = meinText1.join("");
+join3 = meinText1.join(" ");
+join4 = meinText1.join("+");
+
+console.log(join1);
+console.log(join2);
+console.log(join3);
+console.log(join4);
+
+join5 = meinText2.join();
+console.log(join5);
+
+join6 = meinText2.join("");
+console.log(join6);
+
+join7 = meinText2.join(" ");
+console.log(join7);
+
+join8 = meinText2.join("+");
+console.log(join8);
+
+join9 = meinText3.join();
+console.log(join9);
+
+join10 = meinText3.join("");
+console.log(join10);
+
+join11 = meinText3.join(" ");
+console.log(join11);
+
+join12 = meinText3.join("+");
+console.log(join12);
